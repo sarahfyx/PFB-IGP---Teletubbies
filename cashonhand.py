e10b31b91d39dacbdd544cbeab97f93cbf36909b
sarahfyx <s10243474@connect.np.edu.sg>
2023-02-01 16:33:05 +0800

    made changes to comments

--- cashonhand.py
+++ cashonhand.py
@@ -7,25 +7,23 @@ import csv
 fp_read = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 
 # setup filepath for writing
-fp_write = Path.cwd()/"csv_reports"/"summary_report.txt"
+fp_write = Path.cwd()/"summary_report.txt"
 
 # creating above txt file
 fp_write.touch()
 
-
+# defining the function
 def cashonhand():
-    """This function reads the cash-on-hand.csv and writes the highest overhead value to the summary_report.txt file."""
+    """This function reads the cash-on-hand.csv and writes the cash deficit values to the summary_report.txt file."""
     # setting up filepath for reading and writing once again
     fp_read = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
     fp_write = Path.cwd()/"summary_report.txt"
     # creating above txt file
     fp_write.touch()
 
-
-# creating an empty list
+# creating 2 empty lists
 empty_list1 = []
 empty_list2 = []
-
 
 # open file with .open() to return a file object
 with fp_read.open(mode ='r', encoding = "UTF-8", newline = "") as file:
@@ -35,23 +33,24 @@ with fp_read.open(mode ='r', encoding = "UTF-8", newline = "") as file:
     next(reader)
     # iterating each row with a for loop
     for row in reader:
-        # converting the value in the third row (Day 41) to a float and append it to the empty list
+        # converting the values in the first row (day) to a float and append it to the empty list
         empty_list1.append(float(row[0]))
+        # converting the values in the second row (cash on hand) to a float and append it to the empty list
         empty_list2.append(float(row[1]))
 
 
 # assigning the value of the 2nd item (41.0) to day_1
 day_1 = empty_list1[1]
-# assigning the result of subtracting day 41's value from day 40 to cash_deficit1
+# assigning the result of subtracting day 41's cash on hand value from day 40's value on to cash_deficit1
 cash_deficit1 = empty_list2[0] - empty_list2[1]
 # assigning the value of the 9th item (48.0) to day_2
 day_2 = empty_list1[8]
-# assigning the result of subtracting day 48's value from day 47 to cash_deficit1
+# assigning the result of subtracting day 48's cash on hand value from day 47 to cash_deficit2
 cash_deficit2 = empty_list2[7] - empty_list2[8]
 
 
 # using mode = "a" to append the cash deficit (scenario 2) to summary_report.txt
 with fp_write.open(mode = "a") as file:
-    # using writelines method to write the 2 lines for cash deficit (scenario 2) to summary_report.txt
+    # using writelines method to append the 2 lines for cash deficit (scenario 2) to summary_report.txt
     file.writelines(f"\n[CASH DEFICIT] DAY: {empty_list1[1]} , AMOUNT: USD{cash_deficit1}"
     f"\n[CASH DEFICIT] DAY: {empty_list1[8]} , AMOUNT: USD{cash_deficit2}")
--- profitloss.py
+++ profitloss.py
@@ -3,19 +3,16 @@ from pathlib import Path
 # Import csv module
 import csv
 
-
 # setup filepath for reading
 fp_read = Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
-
 
 # setup filepath for writing
 fp_write = Path.cwd()/"summary_report.txt"
 
-
 # creating above txt file
 fp_write.touch()
 
-
+# defining the function
 def profitloss():
     """This function reads the profit-and-loss-usd.csv and writes the profit deficit values to the summary_report.txt file."""
     # setting up filepath for reading and writing once again
@@ -38,20 +35,20 @@ with fp_read.open(mode ='r', encoding = "UTF-8", newline = "") as file:
     next(reader)
     # iterating each row with a for loop
     for row in reader:
-        # converting the value in the first row (day) to a float and append it to the empty list
+        # converting the values in the first row (day) to a float and append it to the empty list
         empty_list1.append(float(row[0]))
-        # converting the value in the fifth row (net profit) to a float and append it to the empty list
+        # converting the values in the fifth row (net profit) to a float and append it to the empty list
         empty_list2.append(float(row[4]))
 
 
 # assigning the value of the 2nd item (41.0) to day
 day = empty_list1[1]
 
-
+# assigning the result of subtracting day 41's net profit from day 40's net profit to profit_deficit
 profit_deficit = empty_list2[0] - empty_list2[1]
 
 
-# using mode = "a" to append the cash deficit (scenario 2) to summary_report.txt
+# using mode = "a" to append the profit deficit (scenario 2) to summary_report.txt
 with fp_write.open(mode = "a") as file:
-    # using writelines method to write the 2 lines for cash deficit (scenario 2) to summary_report.txt
+    # using writelines method to append the profit deficit (scenario 2) to summary_report.txt
     file.writelines(f"\n[PROFIT DEFICIT] DAY: {empty_list1[1]}, AMOUNT: USD{profit_deficit}")
